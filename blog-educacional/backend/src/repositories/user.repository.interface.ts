29bb100bb6405e30a1ca3a2a1c6ff8aed3c84049
Felipe Silva <felipep.silva5741@hotmail.com>
2026-07-12 22:41:50 -0300

    Corrigindo erros de tipagem

--- blog-educacional/backend/src/repositories/user.repository.interface.ts
+++ blog-educacional/backend/src/repositories/user.repository.interface.ts
@@ -3,8 +3,8 @@ import { IUser } from '@/entities/models/user.interface';
 export interface IUserRepository {
     create(user: IUser): Promise<IUser | undefined>
     findAll(): Promise<IUser[]>
-    findById(id: number): Promise<IUser | undefined>
-    findByUsername(username: string): Promise<IUser | undefined>
-    update(id: number, user: Partial<IUser>): Promise<IUser | undefined>
+    findById(id: number): Promise<IUser | null>
+    findByUsername(username: string): Promise<IUser | null>
+    update(id: number, user: Partial<IUser>): Promise<IUser | null>
     delete(id: number): Promise<boolean>
 }
--- blog-educacional/backend/src/use-cases/signin.ts
+++ blog-educacional/backend/src/use-cases/signin.ts
@@ -1,9 +1,10 @@
+import { IUser } from '@/entities/models/user.interface';
 import { IUserRepository } from '@/repositories/user.repository.interface';
 import { InvalidCredentialsError } from './errors/invalid-credentials-errors';
 
 export class SignInUseCase {
     constructor(private userRepository: IUserRepository) { }
-    async handler(username: string): Promise<boolean> {
+    async handler(username: string): Promise<IUser> {
         const user = await this.userRepository.findByUsername(username);
 
         if (!user) {
